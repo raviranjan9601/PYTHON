3f5cc23fb6a5bb05df98084b807d027b25383179
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-08 19:39:27 +0530

    Refactor square area calculation and remove unused arithmetic operations

--- python-2.py
+++ python-2.py
@@ -1,32 +1,8 @@
-# operators
+# task 2
+# write a program to input side of a square & print its area.
 
+area = float(input("Enter a number to find area of Squre : "))
 
-# artimatic operators
+squre = area * area 
 
-a = 15
-b = 10
-
-sum = a + b
-
-subtraction = a - b
-
-multiplaction = a * b
-
-division = a / b
-
-division1 = a // b
-
-division2 = a % b
-
-print(sum)
-
-print(subtraction)
-
-print(multiplaction)
-
-print(division)
-
-print(division1)
-
-print(division2)
-
+print(squre)
