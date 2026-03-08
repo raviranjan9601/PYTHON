118ec23c6419beb8687ba38c442458a97b6c5bf1
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-09 01:12:52 +0530

    Add program to demonstrate dollar sign usage and manipulation

---
+++ python-7.py
@@ -0,0 +1,11 @@
+doller = """\nThe dollar sign($) dates back to the 18th century. Historians believe it came from the abbreviation of the Spanish peso, which was widely used in the Americas before the United States created its own currency.
+
+Merchants often wrote “PS” to represent pesos. Over time, the letters overlapped, forming a symbol that looked like an S with a vertical line through it — the dollar sign $.
+
+Today, the dollar sign($) is mainly associated with the United States Dollar($) (USD), one of the most powerful and widely used currencies in the world. It is also used in countries like Canada, Australia, Singapore, and several others."""
+
+print(doller)
+
+print(doller.count("$"))
+
+print(doller.replace("$","@"))
