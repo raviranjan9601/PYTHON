21621cb9b1fcbec49988c1c2eaa5f5f2e0f840b3
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-09 02:44:10 +0530

    Add program to find the greatest value among three numbers

---
+++ python-10.py
@@ -0,0 +1,17 @@
+# find the Gratest Value.
+
+num1 = float(input("Enate a Number: "))
+num2 = float(input("Ender Second Number: "))
+num3 = float(input("Enter third Number: "))
+
+if(num1 > num2 and num1 > num3):
+    print("frist Number is Greater", num1)
+
+elif(num2 > num1 and num2 > num3):
+    print("second number is grater", num2)
+
+elif(num3 > num2 and num3 > num1):
+    print("third number is grater", num3)
+else:
+    print("Some thing Wrong")
+
