1824ed5c47721482a188ef4a78bcb8f5ee09a647
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-08 22:37:52 +0530

    Add Simple Quiz Game program

---
+++ Simple Quiz Game.py
@@ -0,0 +1,10 @@
+# Game 2: Simple Quiz Game
+
+print("Times to Play Quiz Game")
+
+name = input("Who's Developed Python: ") .lower
+
+if name == "guido van rossum":
+    print("You are Correct")
+else:
+    print("You are Wrong")
