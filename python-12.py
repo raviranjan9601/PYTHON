cd71be2f869691016f4bbd267c55273c7e0d2754
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-09 21:16:02 +0530

    Add program to check if a list contains a palindrome

--- python-12.py
+++ python-12.py
@@ -1,2 +1,18 @@
 # write a program to cheak if a list contains a palindrome of element.
 
+palindrome = []
+
+palindrome.append(input("Enter a Number:"))
+palindrome.append(input("Enter a Number:"))
+palindrome.append(input("Enter a Number:"))
+palindrome.append(input("Enter a Number:"))
+palindrome.append(input("Enter a Number:"))
+
+palindrome1 = palindrome.copy()
+
+palindrome1.reverse()
+
+if (palindrome == palindrome1):
+    print("It is a Palindrome List.")
+else:
+    print("It is Not a Palindrom list")
