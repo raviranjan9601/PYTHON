e2350a3f2aadb396d444b7479ffc9dc7880d403a
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-09 17:17:58 +0530

    Add initial implementation of a login system

---
+++ Login System.py
@@ -0,0 +1,13 @@
+# Mini Project: Login System
+
+print("---Login---")
+
+user_name = input("Enter Your Username: ")
+
+password = input("Enter Your Password: ")
+
+if user_name == "Ravi@123" and password == "Ravirr@1234":
+    print("Login Successful.")
+
+else:
+    print("Invalid Login id or Password.")
