563c3c43771e805cfe30a424866e37e460565ee7
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-09 17:24:03 +0530

    Add implementation of the Word Guess Game

---
+++ Word Guess Game.py
@@ -0,0 +1,10 @@
+# Game: Word Guess Game
+
+word = "Python"
+
+user_input = input("Guess The Code Written Formate: ").capitalize()
+
+if user_input == word:
+    print("You Win.")
+else:
+    print("Try Something New.")
