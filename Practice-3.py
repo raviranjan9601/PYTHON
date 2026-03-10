ce9dbda343fd803e2da18c657f27866f7c05358f
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-10 23:03:55 +0530

    Add program to create a tuple of fruits and display first, last, and length

---
+++ Practice-3.py
@@ -0,0 +1,11 @@
+# Create a tuple of 5 fruits.
+# Print:
+# First fruit
+# Last fruit
+# Length of tuple
+
+fruits = ("apple", "Mango", "Banana", "Pineapple", "Orange")
+
+print(fruits[0])
+print(fruits[-1])
+print(len(fruits))
