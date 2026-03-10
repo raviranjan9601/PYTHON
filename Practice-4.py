e30f9b02c0d65b43e79f02cc4a55d125dddecdca
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-10 23:14:06 +0530

    Add list methods practice program to demonstrate various list operations

---
+++ Practice-4.py
@@ -0,0 +1,29 @@
+# List Methods Practice
+
+marks = [45, 67, 89, 23, 90]
+
+print(marks)
+
+marks.append(45)
+
+print(marks)
+
+marks.insert(3,55)
+
+print(marks)
+
+marks.pop(-1)
+
+print(marks)
+
+marks.remove(55)
+
+print(marks)
+
+marks.sort()
+
+print(marks)
+
+marks.reverse()
+
+print(marks)
