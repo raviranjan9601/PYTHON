588ee930398deef3dfeb55077c9a2e6180e2fc37
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-10 22:56:12 +0530

    Add list slicing practice program to demonstrate slicing techniques

---
+++ Practice-2.py
@@ -0,0 +1,12 @@
+# List Slicing Practice
+
+# Print:
+# First 3 numbers
+# Last 2 numbers
+# Middle numbers
+
+list1 = [10, 40, 45, 67, 75, 36, 15]
+
+print(list1[:3])
+print(list1[5:])
+print(list1[1:-1])
