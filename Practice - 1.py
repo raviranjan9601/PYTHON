534d0308e5a35d960f3e3b92a5620d0fc932a658
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-10 21:59:38 +0530

    Add program to take 5 numbers from the user and store them in a list

---
+++ Practice - 1.py
@@ -0,0 +1,12 @@
+# Takes 5 numbers from the user and store in a list.
+
+num1 = []
+num1.extend(input("Enter 5 number to store: ").split())
+
+# print(num1)
+
+
+# this is work but make list under list
+# num1.append(input("Enter a number: ").split())
+
+print(num1)
