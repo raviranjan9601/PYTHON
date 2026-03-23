c7f6dbe0783d1bf68d06586f2cc36b8cace42aff
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-24 00:09:52 +0530

    List + Dictionary

---
+++ python-19.py
@@ -0,0 +1,35 @@
+# List + Dictionary
+
+mark = {}
+
+sub = input("Enter your Subject Name: ")
+marks = int(input("Enter your Number Name: "))
+
+mark.update({sub:marks})
+
+sub = input("Enter your Subject Name: ")
+marks = int(input("Enter your Number Name: "))
+
+mark.update({sub:marks})
+
+sub = input("Enter your Subject Name: ")
+marks = int(input("Enter your Number Name: "))
+
+mark.update({sub:marks})
+
+sub = input("Enter your Subject Name: ")
+marks = int(input("Enter your Number Name: "))
+
+mark.update({sub:marks})
+
+print(mark)
+
+
+
+
+l1 = [mark]
+
+print(l1)
+
+print("", max(mark.values()))
+print("", min(mark.values()))
