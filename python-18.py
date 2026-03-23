da026fabb41f6d138dcf48e77c514da602fd9a58
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-23 23:38:21 +0530

    Update Dictionary

---
+++ python-18.py
@@ -0,0 +1,18 @@
+# Update Dictionary
+
+raw_data = {
+    "Name" : "Ravi Ranjan Kumar",
+    "age" : 21,
+    "Mob" : 8210509601
+
+}
+
+print(raw_data)
+
+raw_data.update({"City" : "Begusari"})
+
+print(raw_data)
+
+# Dictionary Length
+
+print(len(raw_data))
