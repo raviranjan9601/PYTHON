a40e6a601b359d061634e035b99d3b06415a184c
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-25 22:15:43 +0530

    dictinary from List

---
+++ python-21.py
@@ -0,0 +1,21 @@
+# # set
+
+# numbers = [1, 2, 1, 3, 4, 5, 6, 5, 5, 7, 1, 2, 4, 6, 45, 4, 4, 3, 2, 1, 45, 5, 1, 2, 3, 4, 5,]
+# # remove all duplicates.
+
+# num = set(numbers)
+
+# print(num)
+
+# Unic name 
+
+
+listtt=["Ravi", "PUJA", "puja", "Puja", "ravi", "RAVI", "Amit", "amit", "AMIT", "SONU", "sonu", "Sonu", ]
+
+final= set(listtt)
+
+print(listtt)
+
+print(final)
+
+
---
+++ python-22.py
@@ -0,0 +1,17 @@
+# Dictionary from List
+
+key = ["Name", "Age", "DOB", "City",]
+Value = ["Ravi", 21, "20-01-2004", "Begusari"]
+
+dict1 = {
+    key[0] : Value[0],
+    key[1] : Value[1],
+    key[2] : Value[2],
+    key[3] : Value[3],
+}
+
+print(dict1)
+
+
+
+
