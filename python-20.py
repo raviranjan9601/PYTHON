5ae2b996f604e7741452be7db76a085f8a112334
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-25 21:53:12 +0530

    create a set

---
+++ python-20.py
@@ -0,0 +1,13 @@
+# Favorite Subjects
+
+sub1 = input("Enter Your f. subject name: ")
+sub2 = input("Enter Your f. subject name: ")
+sub3 = input("Enter Your f. subject name: ")
+
+subb = set()
+
+subb.add(sub1)
+subb.add(sub2)
+subb.add(sub3)
+
+print(subb)
