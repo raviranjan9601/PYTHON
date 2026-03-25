222bbd7a2786625fc4570ba90b6e62e2e97c73d5
Ravi Ranjan <raviranjan9601@gmail.com>
2026-03-25 23:02:17 +0530

    slice list in to two part at middle

---
+++ Practice-5.py
@@ -0,0 +1,14 @@
+# slice list in to two part at middle 
+
+list1 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,]
+
+lenth = len(list1)
+
+print(lenth)
+
+devide = int(lenth / 2)
+
+print(devide)
+print(list1)
+print(list1[0:devide])
+print(list1[devide:-1])
