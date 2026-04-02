ecbaeaae1dec95d0f1a38fd766c8dfea5c1cebbd
raviranjan9601 <raviranjan9601@gmail.com>
2026-04-02 20:28:28 +0530

    add initial scripts for looping and printing examples

---
+++ practice-6.py
@@ -0,0 +1,15 @@
+# # Loop 
+
+# i = 0
+# while(i<=100):
+#     print(i)
+#     i+= 1
+
+# table of 2
+
+table = 2
+i = 1
+
+while(i<=10):
+    print(f"{table}x{i} = {table*i}")
+    i+=1
--- python-25.py
+++ python-25.py
@@ -1,7 +1,14 @@
-# loop
+# # loop
 
-# print numbers from 1 to 100
-conunt = 1
-while conunt <= 100:
-    print(conunt)
-    conunt += 1
+# # print numbers from 1 to 100
+
+
+# conunt = 1
+# while conunt <= 100:
+#     print(conunt)
+#     conunt += 1
+
+i = 1
+while(i < 20):
+    print("Hello") 
+    i += 1
